1444495d4a09cd202a1f52c92cd5ac44ec4bd2e9
Darnell Dugger <darnelldugger@gmail.com>
2022-04-27 23:09:07 -0400

    shorten about men

--- components/about-me.js
+++ components/about-me.js
@@ -5,7 +5,7 @@ export default function AboutMe() {
     <>
       <h2 className={`${utilStyles.headingLg} ${utilStyles.aboutTitle}`}>About Me
       </h2>
-      <p className={utilStyles.whiteText}>I am a Full Stack Software Engineer that loves fishing and barbecueing! When I'm not coding, I love spending my time with family and the outdoors. I truly enjoy learning, taking on new projects, contributing to a team and solving problems. Above is a selfie of my son and I while driving our boat in search of big bass!
+      <p className={utilStyles.whiteText}>I am a Full Stack Software Engineer that loves fishing and barbecueing! When I'm not coding, I love spending my time with family and the outdoors. I truly enjoy learning, taking on new projects, contributing to a team and solving problems. 
       </p>
     </>
   )
